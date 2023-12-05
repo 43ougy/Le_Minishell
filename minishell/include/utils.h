7f17ac9c3bcc17098ca35074da287f9c384dc9c3
43ougy <anael.bougy@gmail.com>
2023-12-05 10:29:52 +0100

    manage utils.h

--- minishell/include/utils.h
+++ minishell/include/utils.h
@@ -1,31 +1,42 @@
 #ifndef UTILS_H
 # define UTILS_H
 
-size_t	_strlen(const char *s);
+/*=============== string =================*/
 int		_strcomp(char *s1, char *s2);
 int		_strcompn(char *s1, char *s2, int n);
 char	*_strjoin(char *s1, char *s2);
-char	**_split(char const *s, char c);
-char	*_itoa(int n);
 char	*_substr(char const *s, unsigned int start, size_t len);
 char	*_strdup(const char *s);
 size_t	_strlcpy(char *dst, const char *src, size_t size);
 char	*_strncpy(char *dest, char *src, int n);
+int		_comp(char *s1, char *s2);
+
+/*=============== utils ==================*/
 int		_atoi(char *str);
 void	_bzero(void *s, size_t n);
-void	*_calloc(size_t count, size_t size);
-int		_comp(char *s1, char *s2);
+size_t	_strlen(const char *s);
+char	**_duplicate_tab(char **tab);
+
+/*=============== check_char =============*/
 int		_is_quotes(char c);
 int		_is_char(char c);
 int		_is_num(char c);
 int		_is_alpha(char c);
 int		_is_limiter(char c);
 int		_is_whitespace(char c);
-char	**_duplicate_tab(char **tab);
+
+/*=============== alloc ==================*/
 //void	_free_args(t_prompt *data, int status);
 void	_free_tab(char **tab);
 void	_free(char *string);
+void	*_calloc(size_t count, size_t size);
+
+/*=============== path ===================*/
 char	*_getenv(char **env, char *path_name);
 char	**_give_path(char *path);
+
+char	*get_line(int fd);
+char	**_split(char const *s, char c);
+char	*_itoa(int n);
 
 #endif
